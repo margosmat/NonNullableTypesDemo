74ada5f5b82536448bed543a1f86481ce2e62cfc
margosmat <mateuszmargos@gmail.com>
2019-02-28 19:44:32 +0100

    Add project files.

---
+++ NonNullableTypesDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+#nullable enable
+namespace NonNullableTypesDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var user = new User("Jan", "Kowalski");
+            Console.WriteLine($"{user.FirstName} {user.LastName} {user.Boss.FirstName}");
+        }
+    }
+
+    class User
+    {
+        public User(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public string FirstName { get; set; }
+        public string? LastName { get; set; }
+        public User Boss { get; set; }
+    }
+}
